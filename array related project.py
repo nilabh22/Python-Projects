a9b91cbe6e0a2a20b61061e6bf2c3d9983d02a8f
Nilabh Sahu <59756708+nilabh22@users.noreply.github.com>
2021-07-27 20:55:23 +0530

    Add files via upload

---
+++ array related project.py
@@ -0,0 +1,18 @@
+# It will ask user to enter the no. of days for which user wants temperature data
+temp= int(input("How many days average temperature you want: "))
+sum = 0
+templist =[]
+for i in range(1,  temp+1):
+    daytemp = int(input("Enter Day" + str(i) + "'s temperature: "))
+    templist.append(daytemp)
+    sum +=daytemp
+avg = sum/temp
+print(avg)
+
+avgcount = 0
+for j in templist:
+    if j>avg:
+        avgcount+=1
+print(str(avgcount) +" days temperture is above average temperature")
+    
+
